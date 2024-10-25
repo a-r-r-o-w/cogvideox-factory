6e08a4754843108276b75da421accb0388f88ea1
zR <2448370773@qq.com>
2024-10-25 22:22:07 +0800

    Update test_lora_inference.py

--- tests/test_lora_inference.py
+++ tests/test_lora_inference.py
@@ -2,7 +2,7 @@
 Run this test in Lora adpater checking:
 
 ```shell
-python3 test_lora_inference.py --prompt "Your prompt" --lora_path "path/to/lora" --lora_name "lora_adapter" --output_file "output.mp4" --fps 8
+python3 test_lora_inference.py --prompt "A girl is ridding a bike." --model_path "THUDM/CogVideoX-5B" --lora_path "path/to/lora" --lora_name "lora_adapter" --output_file "output.mp4" --fps 8
 ```
 
 """
@@ -13,8 +13,8 @@ from diffusers import CogVideoXPipeline
 from diffusers.utils import export_to_video
 
 
-def generate_video(prompt, lora_path, lora_name, output_file, fps):
-    pipe = CogVideoXPipeline.from_pretrained("THUDM/CogVideoX-5b", torch_dtype=torch.bfloat16).to("cuda")
+def generate_video(model_path, prompt, lora_path, lora_name, output_file, fps):
+    pipe = CogVideoXPipeline.from_pretrained(model_path, torch_dtype=torch.bfloat16).to("cuda")
     pipe.load_lora_weights(lora_path, weight_name="pytorch_lora_weights.safetensors", adapter_name=lora_name)
     pipe.set_adapters([lora_name], [1.0])
     pipe.enable_model_cpu_offload()
@@ -28,6 +28,7 @@ def generate_video(prompt, lora_path, lora_name, output_file, fps):
 def main():
     parser = argparse.ArgumentParser(description="Generate video using CogVideoX and LoRA weights")
     parser.add_argument("--prompt", type=str, required=True, help="Text prompt for the video generation")
+    parser.add_argument("--model_path", type=str, default="THUDM/CogVideoX-5B", help="Base Model path or HF ID")
     parser.add_argument("--lora_path", type=str, required=True, help="Path to the LoRA weights")
     parser.add_argument("--lora_name", type=str, default="lora_adapter", help="Name of the LoRA adapter")
     parser.add_argument("--output_file", type=str, default="output.mp4", help="Output video file name")
