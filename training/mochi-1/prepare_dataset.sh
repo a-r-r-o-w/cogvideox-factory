0011fa140644dbd45d4931386b02c97822647f7b
sayakpaul <spsayakpaul@gmail.com>
2024-11-29 13:34:05 +0530

    better command

--- training/mochi-1/prepare_dataset.sh
+++ training/mochi-1/prepare_dataset.sh
@@ -6,6 +6,10 @@ OUTPUT_DIR=videos_prepared
 NUM_FRAMES=37
 RESOLUTION=480x848
 
+# Extract width and height from RESOLUTION
+WIDTH=$(echo $RESOLUTION | cut -dx -f1)
+HEIGHT=$(echo $RESOLUTION | cut -dx -f2)
+
 python trim_and_crop_videos.py $VIDEO_DIR $OUTPUT_DIR --num_frames=$NUM_FRAMES --resolution=$RESOLUTION --force_upsample
 
-CUDA_VISIBLE_DEVICES=$GPU_ID python embed.py $OUTPUT_DIR --shape=37x480x848
+CUDA_VISIBLE_DEVICES=$GPU_ID python embed.py $OUTPUT_DIR --shape=${NUM_FRAMES}x${WIDTH}x${HEIGHT}
